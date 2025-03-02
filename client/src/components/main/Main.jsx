925b03668d7e28d31880bcaf9659d94101487e22
ViktorStoichev <viktor.stoichev888@abv.bg>
2025-03-02 12:39:08 +0200

    Split the application into components

---
+++ client/src/components/main/Main.jsx
@@ -0,0 +1,27 @@
+import Pagination from "./Pagination";
+import Search from "./Search";
+import UserList from "./UserList";
+
+export default function Main() {
+    return (
+        <>
+            <main className="main">
+                {/* Section component  */}
+                <section className="card users-container">
+                    {/* Search bar component */}
+                    <Search />
+                    {/* Table component */}
+                    <UserList />
+                    {/* Pagination component  */}
+                    <Pagination />
+                </section>
+                {/* User details component  */}
+
+                {/* Create/Edit Form component  */}
+
+                {/* Delete user component  */}
+
+            </main>
+        </>
+    );
+}
